1a61db2d3c29331a5991034bccd797bd98dbc4a9
Preda Mihai <mihai1.preda@orange.com>
2020-05-19 23:26:02 +0300

    Initial commit

---
+++ worker.js
@@ -0,0 +1,29 @@
+importScripts('lindenmayer.min.js');
+var lsystem = new LSystem({});
+var timeout = {};
+
+onmessage = function(e) {
+
+  // wait a few ms to start thread, to be able to cancel old tasks
+  clearTimeout(timeout);
+  timeout = setTimeout(function() {
+      lsystem.clearProductions();
+      lsystem.setAxiom(e.data.axiom);
+      for (let p of e.data.productions) {
+        if(p.parsejs) {
+          lsystem.setProduction(p.from, eval(p.to));
+        } else {
+          lsystem.setProduction(p.from, p.to);
+        }
+      }
+      console.log(e.data);
+      lsystem.iterate(parseInt(e.data.iterations));
+      console.log('iterated!');
+      postMessage({
+        result: lsystem.getString(),
+        initial: e.data
+      });
+
+  }, 20);
+
+};
