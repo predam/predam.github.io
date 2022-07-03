70c9d2dd83a09d79b417d41d96b13fe0d46c9e4a
Preda Mihai <predamihai14@hotmail.com>
2022-07-03 19:26:37 +0300

    add: flashcard random

--- flashcards/src/App.tsx
+++ flashcards/src/App.tsx
@@ -9,14 +9,24 @@ import Card from './components/Card';
 function App() {
   const irregularVerbs = irregular_verbs as IrregularVerb[];
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isRandom, setIsRandom] = useState(true);
 
   const getCurrentVerb = () => {
     return irregularVerbs[currentIndex];
   };
 
   const next = () => {
-    const nextIndex = Math.floor(Math.random() * irregularVerbs.length);
-    setCurrentIndex(nextIndex);
+    if(isRandom) {
+      const nextIndex = Math.floor(Math.random() * irregularVerbs.length);
+      setCurrentIndex(nextIndex);
+      console.log("random")
+      return;
+    }
+    if(irregularVerbs.length - 1 > currentIndex + 1) {
+      setCurrentIndex(value => value + 1);
+      return;
+    }
+    setCurrentIndex(0);
   };
 
   return (
@@ -24,6 +34,12 @@ function App() {
       <div>
         <h2 className="mb-5 font-bold text-3xl text-center">Irregular Verbs Flashcards</h2>
         <Card verb={getCurrentVerb()} next={next} />
+        <div className="form-control mt-5">
+          <label className="label cursor-pointer">
+            <span className="label-text font-semibold">Random</span>
+            <input type="checkbox" className="toggle toggle-primary" checked={isRandom} onClick={() => setIsRandom(value => !value)} />
+          </label>
+        </div>
       </div>
     </div>
   );
